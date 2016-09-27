8547cefe945e89ddee19e96ca29db2c174b902cd
pricetula <pricetula@gmail.com>
2016-09-27 13:43:08 +0300

    Addid website

---
+++ js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $("#owl-desktop").owlCarousel({
+    // Most important owl features
+    items : 1,
+    itemsCustom : false,
+    singleItem : true,
+    autoPlay : true,
+    stopOnHover : false,
+    navigation : false,
+    scrollPerPage : false,
+    autoHeight : false,
+    pagination : false,
+  });
+
+});
